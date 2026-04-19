608ca1de564b622e73a728f50301d6d0bd2ecd30
bhanu-coder514 <bhanupanditg220@gmail.com>
2026-04-19 12:18:09 +0530

    added edit feature && store todos in local stroage

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Today, I built a login and signup form. I implemented basic input handling and m
 I also created a todo page where I can add and delete tasks. The tasks are stored in state and displayed dynamically using map.
 
 This helped me practice state management, event handling, and working with dynamic data in React.
+
+
+Today, I worked on improving my Todo application.
+
+I implemented localStorage to persist tasks, so the data remains even after refreshing the page. I also debugged and fixed issues related to incorrect JSON parsing.
+
+In addition, I added features to edit, delete, and mark tasks as completed. The completed tasks are visually updated using conditional styling.
+
+This helped me understand state management, handling arrays, working with localStorage, and debugging errors in React.
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,12 +1,26 @@
 
-import React, { useState } from 'react'
+import { list } from 'postcss';
+import React, { useEffect, useState } from 'react'
 
 const Todolist = () => {
 
-    const [lists, setLists] = useState([]);
+    const [lists, setLists] = useState(() => {
+        const data = localStorage.getItem("todos");
+        return data ? JSON.parse(data) : [];
+    });
+
     const [task, setTask] = useState("");
+    const [CompletedTask,setCompletedTask] = useState(() => {
+        const idxs = localStorage.getItem("completedTasks");
+        return idxs ? JSON.parse(idxs) : [];
+    });
+
+    console.log(CompletedTask)
 
     const addtask = () => {
+
+        if(!task) return;
+
         setLists([...lists, task]);
         setTask("");
     }
@@ -16,32 +30,69 @@ const Todolist = () => {
         setLists(newList);
     }
 
+    const editTask = (index) => {
+        const newtxt = prompt("Edit task: ");
+
+        if(!newtxt) return;
+
+        const updatedlist = [...lists];
+        updatedlist[index] = newtxt;
+        setLists(updatedlist);
+    }
+
+    const completeTask = (index) =>{
+
+        if(CompletedTask.includes(index)){
+            setCompletedTask(CompletedTask.filter(i => i != index));
+        }
+        else{
+            setCompletedTask([...CompletedTask,index]);
+        }
+    }
+
+    // add data on local storage when todos changes 
+    useEffect(() =>{
+        localStorage.setItem("todos",JSON.stringify(lists));
+        localStorage.setItem("completedTasks",JSON.stringify(CompletedTask));
+    },[lists,CompletedTask])
+
     return (
         <div className='min-h-screen bg-purple-900 flex flex-col items-center'>
             <h1 className='text-white text-6xl'>Todo List</h1>
 
             <div>
-                <input
+                {/* <input
                     type='text'
                     placeholder='search todos'
                     className='px-3 py-1.5 bg-purple-950 border border-purple-950 mt-10 w-xl rounded-sm text-white'
-                />
+                /> */}
 
                 <ul className='text-white text-2xl font-bold bg-purple-800 mt-10 p-3 space-y-3 rounded-lg'>
 
                     {lists.map((list, index) => {
                         return (
                             <li 
-                                className='bg-purple-700 p-3 rounded flex justify-between'
+                                className='bg-purple-700 p-3 rounded flex justify-between items-center'
                                 key={index}
                             >
                                 
-                                <span>{list}</span>
+                                <span className={CompletedTask.includes(index) ? "line-through" : ""}>{list}</span>
                                 
+                                <div className='flex gap-2'>
+                                <button 
+                                    className='cursor-pointer'
+                                    onClick={() => editTask(index)}
+                                >✒️</button>
                                 <button 
                                     className='cursor-pointer'
                                     onClick={() => deleteTask(index)}
                                 >✂️</button>
+
+                                <button 
+                                    className='cursor-pointer'
+                                    onClick={() => completeTask(index)}
+                                >✔️</button>
+                                </div>
                             </li>
                         )
                     })}
