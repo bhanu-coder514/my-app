46d49e980ea1578dd0f0cf5a45074f739e766f65
bhanu-coder514 <bhanupanditg220@gmail.com>
2026-04-25 11:06:08 +0530

    added infinite scroll feature

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,8 @@ const Card = ({title,description,brand,price,image}) => {
   return (
     <div className='p-2 m-4 w-64 border border-white rounded-2xl shadow-2xl cursor-pointer'>
       <img 
-        src={image} alt="images"
+        src={image} 
+        alt="images"
         className='w-60 h-60'
       />
 
--- src/components/InfiniteScroll.jsx
+++ src/components/InfiniteScroll.jsx
@@ -1,44 +1,95 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Card from './Card';
 
 const InfiniteScroll = () => {
 
-  const [Products,SetProducts] = useState([]);
+  const [Products, SetProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page,setPage] = useState(1);
+  const [HasMore,setHasMore] = useState(true);
+  const lastElementRef = useRef(null);
+  const loadingRef = useRef(false);
 
   // console.log(Products[1]);
 
   useEffect(() => {
-    getData();
-  },[])
 
-  const getData = async () =>{
-    const data = await fetch("https://dummyjson.com/products?limit=20&skip=0");
+    console.log("observer useEffect call");
+
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && !loadingRef.current && HasMore) {
+        setPage((prev) => prev + 1);
+      }
+    });
+
+    if (lastElementRef.current) {
+      observer.observe(lastElementRef.current);
+    }
+
+    
+    return () => observer.disconnect();
+
+  }, [])
+
+  useEffect(() => {
+
+    if(!HasMore) return;
+
+    console.log("api useEffect call");
+
+    getData();
+  }, [page])
+
+  useEffect(() => {
+    loadingRef.current = loading;
+  },[loading])
+
+  const getData = async () => {
+
+    if(loading) return;
+
+    setLoading(true);
+
+    const data = await fetch(`https://dummyjson.com/products?limit=20&skip=${(page - 1) * 20}`);
     const json = await data.json();
 
-    SetProducts(json.products);
+    if(json.products.length === 0){
+      setHasMore(false);
+    }
+
+    SetProducts((prev) => [...prev, ...json.products]);
+
+    setLoading(false);
+
     // console.log(json);
-    console.log(json.products[0]);
+    // console.log(json.products[0]);
     // console.log(json.products[0].title);
   }
 
 
   return (
     <>
-    <div className='flex flex-wrap'>
-      {Products.map((Product) => {
-        return(
-        <Card 
-          title={Product?.title} 
-          description={Product.description} 
-          brand={Product.brand} 
-          price={Product.price} 
-          image={Product.images[0]}
-        />
-      )}
-    )};
-    </div>
+      <div className='flex flex-wrap'>
+        {Products.map((Product) => {
+          return (
+            <Card
+              key={Product.id}
+              title={Product?.title}
+              description={Product.description}
+              brand={Product.brand}
+              price={Product.price}
+              image={Product.images[0]}
+            />
+          )
+        }
+        )}
+      </div>
 
-    <div></div>
+      <div
+        ref={lastElementRef}
+        className='h-5 text-center font-bold'
+      >load data</div>
+      {loading && <h1 className='font-bold text-center'>loading.....</h1>}
     </>
   )
 }
