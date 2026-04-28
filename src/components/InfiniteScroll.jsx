8dd22296aff034237dd0d18bf5244dcb65a5db72
bhanu-coder514 <bhanupanditg220@gmail.com>
2026-04-28 16:12:42 +0530

    added search functionality

--- src/components/InfiniteScroll.jsx
+++ src/components/InfiniteScroll.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Card from './Card';
 import Header from './Header';
+import { SEARCH_API } from '../../utils/constants';
 
 const InfiniteScroll = () => {
 
@@ -10,8 +11,11 @@ const InfiniteScroll = () => {
   const [HasMore, setHasMore] = useState(true);
   const lastElementRef = useRef(null);
   const loadingRef = useRef(false);
+  const [query, setQuery] = useState("");
+  const [queryData, setQueryData] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
-  // console.log(Products[1]);
+  console.log(queryData);
 
   useEffect(() => {
 
@@ -45,9 +49,42 @@ const InfiniteScroll = () => {
     loadingRef.current = loading;
   }, [loading])
 
+  useEffect(() => {
+
+    if(query.trim() === ""){
+      //clear data 
+      setQueryData([]);
+      return;
+    }
+
+    // Debouncing
+    const timer = setTimeout(() => {
+      getQueryData();
+      console.log("search api call...");
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [query])
+
+
+  const getQueryData = async () => {
+    const data = await fetch(`https://dummyjson.com/products/search?q=${query}`);
+
+    const json = await data.json();
+
+    const items = json.products.map(item => item.title);
+
+    console.log(items);
+    setQueryData(items);
+    SetProducts(json.products);
+
+  }
+
   const getData = async () => {
 
-    if (loading) return;
+    if (loading || query) return;
 
     setLoading(true);
 
@@ -71,13 +108,39 @@ const InfiniteScroll = () => {
   return (
     <>
 
-      <Header/>
-      
+      <Header />
+
+      <input
+        className='mt-[8%] ml-[30%] border border-black rounded-2xl py-1 px-7 w-xl'
+        type='text'
+        placeholder='search purduct here...'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onFocus={() => setShowSuggestions(true)}
+        onBlur={() => setShowSuggestions(false)}
+      />
+
+      {/* search suggestions */}
+      {showSuggestions && <div className='absolute shadow-2xl ml-[30%] bg-amber-50 w-xl p-2 rounded-lg'>
+        <ul>
+
+          {queryData.map((item) => {
+            return (
+              <li 
+                key={item}
+                className='mt-2 hover:bg-amber-100 cursor-pointer'>{item}</li>
+            )
+          })}
+
+        </ul>
+      </div>}
+
       <div className='flex flex-wrap'>
+
         {Products.map((Product) => {
           return (
             <Card
-              key={Product.id}
+              key={`${Product.id}-${page}`}
               id={Product.id}
               title={Product?.title}
               description={Product.description}
